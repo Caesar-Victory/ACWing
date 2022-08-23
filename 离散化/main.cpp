c96dd3b177a2d00e0f0289be92e08fe330737bf3
Caesar Tylor <419997284@qq.com>
2022-08-23 20:59:16 +0800

    fix: 新增手写unique()函数

--- 离散化/main.cpp
+++ 离散化/main.cpp
@@ -3,71 +3,102 @@
 #include <algorithm>
 
 using namespace std;
-
-// 开辟空间，自定义容器存储特定值
-const int N = 3 * 1e5 + 10;
-// 分别存储所有用过的下标以及存储前缀和
-int a[N], s[N];
-
+// 声明依次为：结构体别名，自定义常量；声明数组，全局变量和声明容器
 typedef pair<int, int> PII;
-vector <PII> add, query;
-vector <int> alls;
 
+const int N = 3 * 1e5 + 10;
+
+int a[N], s[N];
+int n, m;
+
+vector<int> alls;
+vector<PII> add, query;
+
+// 去重 ---> 满足两个条件，不是第一个元素【下标非零】，其次与前一个数不相等
+vector<int> ::iterator unique(vector<int>& a) {
+
+    int j = 0;
+    for (int i = 0; i <= a.size(); i++) {
+
+        if (!i || a[i] != a[i - 1]) {
+            a[j++] = a[i];
+        }
+    }
+
+    return a.begin() + j;
+}
+
+// 在alls数组中提供以二分的方式查找下标，分别为 add & query数组调用，便于快速查找非重复下标进行插值和计算区间和
 int find(int x) {
+
     int l = 0, r = alls.size() - 1;
+
     while (l < r) {
+
         int mid = (l + r) >> 1;
+
         if (alls[mid] >= x) r = mid;
         else l = mid + 1;
     }
-    return r + 1;
 
+    // 前缀和的下标是从1开始的
+    return r + 1;
 }
 
-// 获取输入，对那个位置插入什么数值
 int main() {
 
-    int n, m;
     cin >> n >> m;
 
     for (int i = 0; i < n; i++) {
+
         int x, c;
         cin >> x >> c;
+
+        // 先存储插值数组，分别为下标和被插入的值
         add.push_back({ x, c });
+
+        // 所有用过的下标存放在alls数组中，插值前会进行去重
         alls.push_back(x);
     }
 
-    // 获取输入，需要查询那个区间
     for (int i = 0; i < m; i++) {
 
         int l, r;
         cin >> l >> r;
 
+        // 查询数组成对存储区间
         query.push_back({ l, r });
+
+        // 区间也是下标，因此分两次存入数组
         alls.push_back(l);
         alls.push_back(r);
     }
-    // 对存储下标的数组进行去重
-    sort(alls.begin(), alls.end());
-    alls.erase(unique(alls.begin(), alls.end()), alls.end());
 
-    // 按照插值数组对数组进行插值
+    // 排序后，同值相邻
+    sort(alls.begin(), alls.end());
+    // 异值在前先后顺序，后面全是同值
+    // alls.erase(unique(alls.begin(), alls.end()), alls.end());
+    alls.erase(unique(alls), alls.end());
+
     for (auto item : add) {
 
+        // 下标都需要在alls数组中寻找
         int x = find(item.first);
+        // 此语法相当于 element[0], element[1]
         a[x] += item.second;
     }
 
-    // 处理前缀和，按照查询数组来运算区间和
-
+    // 先计算前缀和，下标的值域为： [1, alls.size()]
     for (int i = 1; i <= alls.size(); i++) s[i] = s[i - 1] + a[i];
+
 
     for (auto item : query) {
 
+        // 先寻找下标，在计算前缀和
         int l = find(item.first), r = find(item.second);
 
+        // l - 1是因为前缀和
         cout << s[r] - s[l - 1] << endl;
     }
-
     return 0;
 }
