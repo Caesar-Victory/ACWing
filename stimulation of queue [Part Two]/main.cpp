57257f2d245522041a579909de7802289d71b655
caesartylor <419997284@qq.com>
2022-08-26 18:41:52 +0800

    feat: add stimulaton of queue and  annotate.

---
+++ stimulation of queue [Part Two]/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 1e5 + 10;
+
+// tt: head of queue, kk: rear of queue.
+// waring: tt is equal to fast pointer, and kk is equal to slow pointer.
+int tt, kk, que[N];
+
+// add element
+
+que[++tt] = x;
+
+// remove element
+++kk;
+
+int main() {
+
+	return 0;
+}
