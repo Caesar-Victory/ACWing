d2110ef9204b43cf3f0aff515380ae96ba0ce55a
Caesar Tylor <419997284@qq.com>
2022-09-29 22:12:02 +0800

    notification: git clone online version fater configuration

--- 位运算【计算当前数字的二进制】/main.cpp
+++ 位运算【计算当前数字的二进制】/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
 	int n = 10;
 	// 需要事先知道10表示为2进制有四位
+	// 因为 10 > 2 ^ 3， 因此有四位
 	for (int k = 3; k >= 0; k--) cout << (n >> k & 1) << endl;
 
 	return 0;
--- 快速排序/源.cpp
+++ 快速排序/源.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // 数组,数组的长度和数组空间上限均声明为全局变量
-// 如果不加 const 静态常量修饰符，N 实际值为 double类型，但申明类型为 int, 报错类型不一致
+// 如果不加 const 静态常量修饰符，N 实际值为 double类型，但声明类型为 int, 报错类型不一致
 const int N = 1e6 + 10;
 
 // 形参，也可以理解为默认值
@@ -17,7 +17,7 @@ void quick_sort(int q[], int l, int r) {
         return;
     }
 
-    // 虚拟指针重定位，便于操作数据
+    // 虚拟指针重定位，取出位序中点的值，便于操作数据
     int j = l - 1, k = r + 1, x = q[(l + r) >> 1];
     //int x = q[l], j = l - 1, k = r + 1;
 
@@ -51,6 +51,7 @@ int main() {
         scanf("%d", &q[i]);
     }
 
+    // 快速排序接受的是下标
     quick_sort(q, 0, n - 1);
 
     // 逐个打印排序后数组中每一个元素
