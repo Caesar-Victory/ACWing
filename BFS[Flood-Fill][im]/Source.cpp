7b4a9e4ac38571b65cbdf1e250fe016b492ff57f
Caesar Tylor <419997284@qq.com>
2022-11-16 17:17:20 +0800

    feat: 对单链表新增部分注释

--- BFS[Flood-Fill][im]/Source.cpp
+++ BFS[Flood-Fill][im]/Source.cpp
@@ -9,7 +9,90 @@
 
 using namespace std;
 
+#include <cstdio>
+#include <iostream>
+
+
+// 这里应当理解为用 x 替换first
+#define x first
+#define y second
+
+using namespace std;
+
 typedef pair<int, int> PII;
+
+const int N = 1e3 + 10, M = N * N;
+
+int n, m;
+// 申请循环使用的队列空间
+PII q[M];
+// 字符地图，收纳所有的 'W' 和普通陆地
+char g[N][N];
+// 判重数组
+bool st[N][N];
+
+void bfs(int sx, int sy) {
+    
+    // 初始化队列的头尾指针
+    int hh=0, tt=0;
+    // 初始化队列和标记数组，初始化队列亦是如此，BFS中的队列本质是换基，初始化只是给它找一个基点
+    q[0] = {sx, sy};
+    // 初始化标记是因为每一次传参就意味着有一个坐标被用过了；
+    st[sx][sy] = true;
+    
+    while (hh <= tt) {
+        // 从队头取出元素，则头指针自增, 取出第一个元素，可以直接取出，下一次访问时再读取自增
+        PII t = q[hh++];
+        // 如何构造八连通量呢？挖去3*3矩阵核心点，保留四周；
+        for (int i = t.x - 1; i <= t.x + 1; i++) {
+            
+            for(int j = t.y - 1; j <= t.y + 1; j++) {
+                
+                // 跳过中心点；在所有坐标中，只有对角线的四个坐标，完全不与中心点的横坐标或者纵坐标相等，
+                // 因此这里只有横纵坐标完全与取出来的中心点相同才会跳过
+                if (i == t.x && j == t.y) continue;
+                // 不能撞墙，也就是超出边界
+                if (i < 0 || i >= n || j < 0 || j >= m) continue;
+                // 不能走到陆地或者搜索已经搜过的地方
+                if (g[i][j] == '.' || st[i][j]) continue;
+                
+                // 从队尾插入元素，则尾指针自增；本次就需要将元素插入进去，因此直接前++
+                q[++tt] = {i, j};
+                st[i][j] = true;
+            }
+        }
+    }
+    
+}
+
+int main(void) {
+    
+    int cnt=0;
+    
+    scanf("%d%d", &n, &m);
+    
+    // 逐行读取输入
+    for (int i = 0; i < n; i++) scanf("%s", g[i]);
+    
+    // for (int i = 0; i < n; i++) printf("%s", g[i]), puts("");
+    
+    for (int i = 0; i < n; i++) {
+        
+        for (int j = 0; j < m; j++) {
+            
+            // 只有当该点是水域，且没有被搜索过
+            if (g[i][j] == 'W' && !st[i][j]) {
+                
+                bfs(i, j);
+                cnt++;
+            }
+        }
+    }
+    
+    printf("%d\n", cnt);
+    
+    return 0;
+}
 
 const int N = 1e3 + 10, M = N * N;
 
--- single linklist[Part Two]/main.cpp
+++ single linklist[Part Two]/main.cpp
@@ -1,52 +1,92 @@
-#define  _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstdio>
 
 using namespace std;
-int n;
-const int N = 10003;
-int h[N], e[N], nxt[N], idx;
 
-void insert(int x) {
+const int N = 1e5 + 10;
 
-	int k = (x % N + N) % N;
-	e[idx] = x;
-	nxt[idx] = h[k];
-	h[k] = idx++;
+// head 头节点的下标
+// e[] 数组依次按照位序存储相应的值
+// nxt[] 数组存储当前节点的下一个节点的位置【等价于位序】
+// 二者在同一个位序相同，因为分别存储了值和nxt指针指向的对象
+int head, idx, e[N], ne[N];
 
+// 初始化，头节点默认为：-1
+void init(void) {
+    head = -1;
+    idx = 0;
 }
 
-bool find(int x) {
+void remove(int k) {
 
-	int k = (x % N + N) % N;
-	for (int i = h[k]; i != -1; i = nxt[i]) {
-		if (e[i] == x) {
-			return true;
-		}
-	}
-	return false;
+    // 当前节点指向当前节点的下一个节点
+    ne[k] = ne[ne[k]];
 }
 
-int main(void) {
+// 从头节点插入
+void add_head(int k) {
 
-	scanf("%d", &n);
+    // e是链表，首先使用idx指针将数值存储起来
+    e[idx] = k;
+    // 利用下标，将【新节点的】nex指针指向头节点[指向的下一个值]
+    // 本来整个数组结构就是单链表，此处代码的含义和哈希函数单链表的意义不一样，后者是一个数组
+    ne[idx] = head;
+    // head 指向建立的新节点 e[idx]，以存值的方式
+    head = idx;
+    idx++;
+}
 
-	while (n--) {
-		int x;
-		char op[2];
-		scanf("%d%s", &x, op);
+// 从某个节点的后面插入
+void add_rear(int k, int x) {
 
-		if (*op == "I") {
-			insert(x);
-		}
-		else {
-			if (find(x) == true) {
-				puts("true");
-			}
-			else {
-				puts("false");
-			}
-		}
-	}
+    // 照例将待插入节点的值，存储起来
+    e[idx] = x;
+    // 可用下标idx推动nex指针，接受K的下一个节点
+    ne[idx] = ne[k];
+    // k的下一个节点与idx交换节点，因为nex数组始终存储下一个节点的下标，而idx已经用过了
+    ne[k] = idx;
+    idx++;
+}
 
+int main() {
+    int m;
+    cin >> m;
+
+    init();
+
+    while (m--) {
+        char flag;
+        cin >> flag;
+
+        if (flag == 'H') {
+
+            int val;
+            cin >> val;
+
+            add_head(val);
+        }
+        else if (flag == 'D') {
+
+            int k;
+            cin >> k;
+
+            //特判：当 k = 0时，意味着删除头节点，那么直接把下一个可插入节点直接指向 在nex数组中head的下一个节点
+            if (!k) head = ne[head];
+
+            remove(k - 1);
+        }
+        else if (flag == 'I') {
+            int k, x;
+            cin >> k >> x;
+
+            add_rear(k - 1, x);
+        }
+
+    }
+
+    // e存储链表，指针推移依靠ne数组，i始终从nex数组中取出下标--->移动指针
+    for (int i = head; i != -1; i = ne[i]) cout << e[i] << " ";
+    cout << endl;
+
+    return 0;
 }
