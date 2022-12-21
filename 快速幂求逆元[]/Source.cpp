db53fc446d2c379392ce7176b6a2048231ff2432
Caesar Tylor <419997284@qq.com>
2022-12-21 18:29:01 +0800

    feat: 快速幂求逆元

---
+++ 快速幂求逆元[]/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long LL;
+
+int quickmi(int a, int b, int p) {
+    int res = 1 % p;
+    while (b) {
+        if (b & 1) res = (LL)res * a % p;
+        b >>= 1;
+        a = (LL)a * a % p;
+    }
+    return res;
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    while (n--) {
+        int a, p;
+        scanf("%d%d", &a, &p);
+        if (a % p) printf("%d\n", quickmi(a, p - 2, p));
+        else puts("impossible");
+    }
+    return 0;
+}
